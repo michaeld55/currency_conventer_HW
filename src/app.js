11aa82bba84e4dea697cdc8d353902d9e76cbfaf
michaeld55 <michaeld55@hotmail.co.uk>
2020-03-31 18:13:06 +0100

    did 1st task

--- src/app.js
+++ src/app.js
@@ -4,19 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data:{
-
+        currencies: {},
+        reversedCurrencies: {},
+        selectedCurrency: null,
     },
 
     computed: {
-
+      flipCurrencies: function(){
+        return this.flip(this.selectedCurrency)
+      }
     },
 
-    mounted: {
-
+    mounted(){
+        this.getCurrencies();
+        // this.flipCurrencies();
     },
-    
+
     methods: {
-
-    }
+        getCurrencies: function(){
+            fetch("https://api.exchangeratesapi.io/latest")
+            .then(response => response.json())
+            .then(currencies => this.currencies = currencies)
+        },
+      }
   })
 })
