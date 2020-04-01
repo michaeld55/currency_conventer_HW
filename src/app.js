2b4a6fe27f95ae4863b374d70a4b05f3591a9faf
michaeld55 <michaeld55@hotmail.co.uk>
2020-04-01 09:38:29 +0100

    rounding works?

--- src/app.js
+++ src/app.js
@@ -35,4 +35,4 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-// Allow cross-currency conversions - from non-base to non-base currencies. (e.g. from GBP to USD)git
+// Allow cross-currency conversions - from non-base to non-base currencies. (e.g. from GBP to USD)
